85c55c3831ca81174da4a713ff78fb04287ab7e3
Jason Kurian <jasonk92@gmail.com>
2016-01-28 18:35:27 -0500

    Draw out the link function for less nesting, better readability

--- ng-fallback-src.js
+++ ng-fallback-src.js
@@ -4,24 +4,24 @@
   /**
    * Creates a fallback for an src when it fails
    */
-  function ngFallbackSrcDirective () {
-    return {
-      restrict: 'A',
-      scope: false,
-      link: function fallbackSrcLink (scope, iElement, iAttrs) {
-        iElement.bind('error', attachFallbackImg);
+  function fallbackSrcLink (scope, iElement, iAttrs) {
+    iElement.bind('error', attachFallbackImg);
 
-        scope.$on('$destroy', function rmFallbackSrcBinding () {
-          iElement.unbind('error', attachFallbackImg);
-        });
+    scope.$on('$destroy', function rmFallbackSrcBinding () {
+      iElement.unbind('error', attachFallbackImg);
+    });
 
-        function attachFallbackImg () {
-          angular.element(iElement).attr('src', iAttrs.fallbackSrc);
-        }
-      }
-    };
+    function attachFallbackImg () {
+      angular.element(iElement).attr('src', iAttrs.fallbackSrc);
+    }
   }
 
   angular.module('ngfallbackSrc', [])
-    .directive('fallbackSrc', ngFallbackSrcDirective);
+    .directive('fallbackSrc', function () {
+      return {
+        restrict: 'A',
+        scope: false,
+        link: fallbackSrcLink
+      };
+    });
 }());
